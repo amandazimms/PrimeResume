c191636d118fa2951a2bb763e066ba48f0509b99
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-07-06 00:53:48 -0500

    bee now moves continuously with WASD. When it 'overlaps' w cosmo flower (within 50px distance), we pring a console.log message. Next is to do the same for the other flowers, and the honeycomb

--- script.js
+++ script.js
@@ -27,34 +27,81 @@ window.onload = function () {
 //HONEY COUNT
 let honeyCount = 0;
 
-function clickFlower() {
+function ClickFlower() {
     honeyCount++;
     console.log("honey count is " + honeyCount);
   }
 
 
-//BEE MOVEMENT
-let bee = document.querySelector('.bee');
-let speed = 10;
 
+//BEE & FLOWER SETUP
+const bee = document.querySelector('.bee');
+const speed = 50;
+
+const cosmos = document.querySelector('.cosmos');
+
+function Shape(x, y){
+    this.x = x;
+    this.y = y;
+    this.r = 50;
+}
+
+//BEE COLLISION WITH FLOWERS
+function CheckDistance(flower) {
+    let beeRect = bee.getBoundingClientRect();
+    let beeHeight = (beeRect.bottom - beeRect.top) /2;
+    let beeWidth = (beeRect.right - beeRect.left) /2;
+
+    let beeX = beeRect.bottom - beeHeight;
+    let beeY = beeRect.right - beeWidth;
+
+    let flowerRect = flower.getBoundingClientRect();
+    let flowerHeight = (flowerRect.bottom - flowerRect.top) /2;
+    let flowerWidth = (flowerRect.right - flowerRect.left) /2;
+
+    let flowerX = flowerRect.bottom - flowerHeight;
+    let flowerY = flowerRect.right - flowerWidth;
+
+    let distToFlower = GetDistance(beeX, beeY, flowerX, flowerY);
+
+    if (distToFlower < 50) {
+        console.log("they are overlapping: " + distToFlower);
+    }
+    else {
+        console.log(distToFlower);
+    }
+}
+
+function GetDistance(x1, y1, x2, y2){
+    let y = x2 - x1;
+    let x = y2 - y1;
+    
+    return Math.sqrt(x * x + y * y);
+}
+
+
+
+//BEE MOVEMENT
 window.addEventListener('load', () => {
     bee.style.position = 'absolute'; 
     bee.style.left = 0;
     bee.style.top = 0;
 });
 
-window.addEventListener('keyup', (e) => {
+window.addEventListener('keydown', (e) => {
+    CheckDistance(cosmos);
+
     switch(e.key) {
-        case 'ArrowLeft':
+        case 'a':
             bee.style.left = parseInt(bee.style.left) - speed + 'px';
             break;
-        case 'ArrowRight':
+        case 'd':
             bee.style.left = parseInt(bee.style.left) + speed + 'px';
             break;
-        case 'ArrowUp':
+        case 'w':
             bee.style.top = parseInt(bee.style.top) - speed + 'px';
             break;
-        case 'ArrowDown':
+        case 's':
             bee.style.top = parseInt(bee.style.top) + speed + 'px';
             break;
     }
