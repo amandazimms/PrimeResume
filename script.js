8cc220001c7eb62688b9fb4ea8a144fc712c254a
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-07-05 23:58:13 -0500

    the bee now moves! followed tutorial, now need to apply Z index and continuous movement

--- script.js
+++ script.js
@@ -31,3 +31,31 @@ function clickFlower() {
     honeyCount++;
     console.log("honey count is " + honeyCount);
   }
+
+
+//BEE MOVEMENT
+let bee = document.querySelector('.bee');
+let speed = 10;
+
+window.addEventListener('load', () => {
+    bee.style.position = 'absolute'; 
+    bee.style.left = 0;
+    bee.style.top = 0;
+});
+
+window.addEventListener('keyup', (e) => {
+    switch(e.key) {
+        case 'ArrowLeft':
+            bee.style.left = parseInt(bee.style.left) - speed + 'px';
+            break;
+        case 'ArrowRight':
+            bee.style.left = parseInt(bee.style.left) + speed + 'px';
+            break;
+        case 'ArrowUp':
+            bee.style.top = parseInt(bee.style.top) - speed + 'px';
+            break;
+        case 'ArrowDown':
+            bee.style.top = parseInt(bee.style.top) + speed + 'px';
+            break;
+    }
+});
